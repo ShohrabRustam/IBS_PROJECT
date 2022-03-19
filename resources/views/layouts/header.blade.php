1a4d9d3de03059d351545cb9d6015b09f9bb5e43
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-20 02:02:26 +0530

    add the page for insurance type

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -101,10 +101,10 @@
                         Insurance Type
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="LifeInsurance"> Life Insurance </a></li>
-                        <li> <a class="dropdown-item" href="HealthInsurance">Health Insurance</a></li>
-                        <li> <a class="dropdown-item" href="CarInsurance"> Car Insurance </a> </li>
-                        <li> <a class="dropdown-item" href="BikeInsurance"> Bike Insurance </a></li>
+                        <li><a class="dropdown-item" href="{{ URL::to('LifeInsurance') }}"> Life Insurance </a></li>
+                        <li> <a class="dropdown-item" href="{{ URL::to('HealthInsurance') }}">Health Insurance</a></li>
+                        <li> <a class="dropdown-item" href="{{ URL::to('CarInsurance') }}"> Car Insurance </a> </li>
+                        <li> <a class="dropdown-item" href="{{ URL::to('BikeInsurance') }}"> Bike Insurance </a></li>
                         <li> <a class="dropdown-item" href="#">More Insurance Blog</a> </li>
                     </ul>
                 </li>
