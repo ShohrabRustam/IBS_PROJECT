dbe414afdf4a7aadbe69778803bfba2b0998c5f3
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-03-20 16:29:16 +0530

    setup the web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+##############################################################################################################################################################
+
+
 // home page
 Route::get('/', function () {
     return view('home');
